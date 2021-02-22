677d0abeb18ce346eb668496a071793875be32b6
Maja Abraham <maja.abraham1@gmail.com>
2021-02-22 16:57:39 +0100

    Ustvarila posebno datoteko s pojmi.

---
+++ uvoz/pojmi.r
@@ -0,0 +1,125 @@
+# vektorji pojmov za prevode podatkov
+
+# vektor držav
+drzave <- c("European Union" = "EU28",
+            "Belgium" = "Belgija",
+            "Bulgaria" = "Bolgarija",
+            "Czechia" = "Češka",
+            "Denmark" = "Danska",
+            "Germany" = "Nemčija",
+            "Estonia" = "Estonija",
+            "Ireland" = "Irska",
+            "Greece" = "Grčija",
+            "Spain" = "Španija",
+            "France" = "Francija",
+            "Croatia" = "Hrvaška",
+            "Italy" = "Italija",
+            "Cyprus" = "Ciper",
+            "Latvia" = "Latvija",
+            "Lithuania" = "Litva",
+            "Luxembourg" = "Luksemburg",
+            "Hungary" = "Madžarska",
+            "Malta" = "Malta",
+            "Netherlands" = "Nizozemska",
+            "Austria" = "Avstrija",
+            "Poland" = "Poljska",
+            "Portugal" = "Portugalska",
+            "Romania" = "Romunija",
+            "Slovenia" = "Slovenija",
+            "Slovakia" = "Slovaška",
+            "Finland" = "Finska",
+            "Sweden" = "Švedska"
+)
+
+# vektor zadnjih spletnih nakupov
+zadnji <- c("in the last 3 months" = "v zadnjih 3. mesecih",
+            "between 3 and 12 months ago" = "med 3. in 12. meseci",
+            "in the 12 months" = "v zadnjih 12. mesecih",
+            "more than a year ago or never" = "pred več kot letom ali nikoli"
+)
+
+# vektor spolov
+spol <- c("All Individuals" = "skupaj",
+          "Individuals, 16 to 24 years old" = "skupaj",
+          "Individuals, 25 to 54 years old" = "skupaj",
+          "Individuals, 55 to 74 years old" = "skupaj",
+          "Males, 16 to 24 years old" = "moški",
+          "Males 25 to 54 years old" = "moški",
+          "Males 55 to 74 years old" = "moški",
+          "Females, 16 to 24 years old" = "ženske",
+          "Females 25 to 54 years old" = "ženske",
+          "Females 55 to 74 years old" = "ženske"
+)
+
+# vektor starostnih skupin
+starost <- c("All Individuals" = "skupaj",
+             "Individuals, 16 to 24 years old" = "16-24",
+             "Individuals, 25 to 54 years old" = "25-54",
+             "Individuals, 55 to 74 years old" = "55-74",
+             "Males, 16 to 24 years old" = "16-24",
+             "Males 25 to 54 years old" = "25-54",
+             "Males 55 to 74 years old" = "55-74",
+             "Females, 16 to 24 years old" = "16-24",
+             "Females 25 to 54 years old" = "25-54",
+             "Females 55 to 74 years old" = "55-74"
+)
+
+# vektor pogostosti spletnega nakupovanja
+pogosto <- c("1 or 2 times" = "1-2",
+             "3 to 5 times" = "3-5",
+             "6 times or more" = "6 ali več"
+)
+
+# vektor vrst blaga
+vrsta <- c("Food/groceries" = "hrana / živila",
+           "clothes, sports goods" = "oblačila / športna oprema",
+           "household goods" = "oprema doma",
+           "films/music or books/magazines/e-learning material or computer software" = 
+             "filmi / glasba / knjige / revije / gradiva za e-učenje / računalniška programska oprema",
+           "computer hardware" = "računalniška strojna oprema",
+           "electronic equipment" = "elektronska oprema",
+           "shares/insurance/other financial services" = "delnice / zavarovanje / druge finančne storitve",
+           "tickets for events" = "vstopnice za prireditve",
+           "travel and holiday accommodation" = "potovanja in počitniške nastanitve",
+           "medecine" = "zdravila",
+           "telecom services" = "telekomunikacijske storitve",
+           "others" = "drugo"
+)
+
+# vektor težav
+tezave <- c("Difficulties concerning guarantees" = "težave s garancijami",
+            "Speed of delivery longer than indicated" = 
+              "hitrost dostave daljša od navedene",
+            "Delivery costs or final price higher than indicated" = 
+              "stroški dostave ali končna cena višja od navedene",
+            "wrong or damaged good/services delivered" = 
+              "napačno ali poškodovano blago / opravljene storitve",
+            "problems with fraud" = "težave z goljufijo",
+            "Complaints and redress were difficult or no satisfactory response received after complaint" =
+              "Težave s pritožbami in odškodninami / po pritožbi ni bilo zadovoljivega odgovora",
+            "Other" = "drugo"
+)
+
+# vektor vrednosti spletnih nakupov
+vrednosti <- c("less than 50 euro " = "49 ali manj",
+               "between 50 and 99 euro " = "50 - 99",
+               "between 100 and 499 euro " = "100 - 499 ",
+               "between 500 and 999 euro " = "500 - 999",
+               "1000 euro or more " = "1000 ali več"
+)
+
+# vektor prodajalcev na spletu
+prodajalci <- c("from national sellers" = "domači",
+                "from sellers from other EU countries" = "iz EU",
+                "from sellers from the rest of the world (non-EU)" = "iz drugih držav"
+)
+
+# vektor regij
+regije <- c("Global" = "globalno",
+            "Poland" = "Poljska",
+            "Europe" = "Evropa",
+            "Netherlands" = "Nizozemska",
+            "France" = "Francija",
+            "Russia" = "Rusija",
+            "Germany" = "Nemčija",
+            "Eastern Europe" = "Vzhodna Evropa")
