9ab576fa48923274e8b87281c8692aff1454a1aa
Maja Abraham <maja.abraham1@gmail.com>
2021-02-22 17:11:38 +0100

    Izbrisala vzorčno kodo.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -177,19 +177,3 @@ barvanje <- zdruzen.prevladujoc %>%
   group_by(drzava) %>% summarise(skupina = str_c(prodajalec, collapse = " in "))
 
 paleta2 <- brewer_pal(type = "div", palette = 9) (4)
-
-
-
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-# Če zemljevid nima nastavljene projekcije, jo ročno določimo
-proj4string(zemljevid) <- CRS("+proj=utm +zone=10+datum=WGS84")
-
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
